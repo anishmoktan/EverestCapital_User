cd7b5bfe66e86553118a79cdb72eeb6b9f7758f4
anishmoktan <anish.tmngo@gmail.com>
2020-11-10 09:41:00 -0500

    added changes to password verification

--- EverestCap_Users/users_functions.py
+++ EverestCap_Users/users_functions.py
@@ -13,8 +13,8 @@ class Users:
         self.columns = ["Firstname", "Lastname", "Email", "Password", "City","Country"]   # providing values for the colmuns
         self.table = self.DB.Table(self.__Tablename__)
     
-    def put(self, username, firstname, lastname, email, city, country, password): #to create account
-        if verify_username_exists(username) and verify_email_exists(email):
+    def put(self, username, firstname, lastname, email, city, country, password): #to post new columns on DynamoDB
+        if username_vacant(username) and email_vacant(email):
             response = self.table.put_item(
                     Item = {
                         self.Primary_Column_Name: username,
@@ -26,18 +26,37 @@ class Users:
                         self.columns[5]: self.hash_password(password)
                     }
                 )
-            return True
-        else:
-            return False
+            return {"Result":True,
+                    "Error": None,
+                    "Description": "Account was successfully created"
+            }
+        elif username_vacant(username) == True and email_vacant(email) == False:
+            return {"Result":False,
+                    "Error": "Vacancy error",
+                    "Description": "Email already exists in the database"
+            }
+        
+        elif username_vacant(username) == False and email_vacant(email) == True:
+            return {"Result":False,
+                    "Error": "Vacancy error",
+                    "Description": "Username already exists in the database"
+            }
 
-    def verify_username_exists(self, username): #checks if username already exists
+        else:
+            return {"Result":False,
+                    "Error": "Database error"
+                    "Description": "Could not verify if username and email already exists in the datase"
+            }
+
+
+    def username_vacant(self, username): #checks if username does not exist in the database
         response = self.table.scan(FilterExpression=Attr("Username").eq(username))
         if response["Items"] == []:
             return True
         else:
             return False
 
-    def verify_email_exists(self, email): #checks if email already exists
+    def email_vacant(self, email): #checks if email does not exist in the database
         response = self.table.scan(FilterExpression=Attr("Email").eq(email))
         if response["Items"] == []:
             return True
@@ -45,7 +64,7 @@ class Users:
             return False
 
     def update_account(self, username, firstname, lastname, email, city, country, password): #change values in the column
-        if verify_username_exists(username) and verify_email_exists(email):
+        if username_vacant(username) and email_vacant(email):
             response = self.table.put_item(
                     Item = {
                         self.Primary_Column_Name: username,
@@ -61,17 +80,28 @@ class Users:
         else:
             return False
 
-    def hash_password(self, password):
-        hashed = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
     
-    def de_hash_password(self, password, hashed):
-        if bcrypt.checkpw(password, hashed):
-            return True
+
+    def password_verification(self,username,password):
+        response = self.table.scan(FilterExpression=Attr("Username").eq(username))
+        
+        if len(response["Items"]) > 0:
+            original_password = response["Items"][0]["password"]
+            if de_hash_password(original_password) == password:
+                return {"Result": True,
+                        "Error": None,
+                        "Description": "Password was verified"}
+            else:
+                return {"Result": False,
+                        "Error": "Password error",
+                        "Description": "Password was not verified"}
         else:
-            return False
+            return {"Result": False,
+                        "Error": "Password error",
+                        "Description": "Password was not verified"}
 
     def update_password(self, username, password):
-        response = self.table.scan(FilterExpression=Attr("username").eq(user)) #get reponse from DynamoDB
+        response = self.table.scan(FilterExpression=Attr("username").eq(username)) #get reponse from DynamoDB
 
         if len(response["Items"]) > 0: # if the response contains a user we bgan to presver dat such as the user city, country, name, etc,
             
@@ -107,7 +137,7 @@ class Users:
 
     def update_username(self, username, password):
         response = self.table.scan(
-            FilterExpression=Attr("username").eq(user)
+            FilterExpression=Attr("username").eq(username)
         )
 
         if len(response["Items"]) > 0: # if the response contains a user we bgan to presver dat such as the user city, country, name, etc,
@@ -156,3 +186,12 @@ class Users:
                  "Result": False,
                  "Error": "Account does not exist in database"
                 }
+
+        def hash_password(self, password):
+        hashed = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+    
+        def de_hash_password(self, password, hashed):
+            if bcrypt.checkpw(password, hashed):
+                return True
+            else:
+                return False
