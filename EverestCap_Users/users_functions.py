7b7218ec736b1935016a7dcf9c058038ddfd394c
anishmoktan <anish.tmngo@gmail.com>
2020-11-11 18:59:46 -0500

    updated the functions file

--- EverestCap_Users/users_functions.py
+++ EverestCap_Users/users_functions.py
@@ -13,106 +13,180 @@ class Users:
         self.columns = ["Firstname", "Lastname", "Email", "Password", "City","Country"]   # providing values for the colmuns
         self.table = self.DB.Table(self.__Tablename__)
     
-    def put(self, username, firstname, lastname, email, city, country, password): #to post new columns on DynamoDB
-        if username_vacant(username) and email_vacant(email):
-            response = self.table.put_item(
-                    Item = {
-                        self.Primary_Column_Name: username,
-                        self.columns[0]: firstname,
-                        self.columns[1]: lastname,
-                        self.columns[2]: email,
-                        self.columns[3]: city,
-                        self.columns[4]: country,
-                        self.columns[5]: self.hash_password(password)
-                    }
-                )
-            return {"Result":True,
-                    "Error": None,
-                    "Description": "Account was successfully created"
-            }
-        elif username_vacant(username) == True and email_vacant(email) == False:
-            return {"Result":False,
-                    "Error": "Vacancy error",
-                    "Description": "Email already exists in the database"
-            }
-        
-        elif username_vacant(username) == False and email_vacant(email) == True:
-            return {"Result":False,
-                    "Error": "Vacancy error",
-                    "Description": "Username already exists in the database"
-            }
+    def put(self, username, firstname, lastname,  email, city, country, password):
 
+        response = self.table.put_item(
+            Item={
+                self.Primary_Column_Name: username,
+                self.columns[0]: firstname,
+                self.columns[1]: lastname,
+                self.columns[2]: email,
+                self.columns[3]: city,
+                self.columns[4]: country,
+                self.columns[5]: self.hash_password(password)
+            }
+        )
+
+    def create_after_verification(self, username, firstname, lastname,  email, city, country, password):
+        if self.username_vacant(username) and self.email_vacant(email):
+            self.put(username, firstname, lastname,  email, city, country, password)
+            return True
         else:
-            return {"Result":False,
-                    "Error": "Database error"
-                    "Description" : "Could not verify if username and email already exists in the datase"
-            }
-
+            return False
 
     def username_vacant(self, username): #checks if username does not exist in the database
         response = self.table.scan(FilterExpression=Attr("Username").eq(username))
+    
         if response["Items"] == []:
             return True
         else:
             return False
 
     def email_vacant(self, email): #checks if email does not exist in the database
-        response = self.table.scan(FilterExpression=Attr("Email").eq(email))
+        response = self.table.scan(
+            FilterExpression = Attr("Email").eq(email)
+            )
         if response["Items"] == []:
             return True
         else:
             return False
-
-    def update_account(self, username, firstname, lastname, email, city, country, password): #change values in the column
-        if username_vacant(username) and email_vacant(email):
-            response = self.table.put_item(
-                    Item = {
-                        self.Primary_Column_Name: username,
-                        self.columns[0]: firstname,
-                        self.columns[1]: lastname,
-                        self.columns[2]: email,
-                        self.columns[3]: city,
-                        self.columns[4]: country,
-                        self.columns[5]: self.hash_password(password)
-                    }
-                )
+    
+    def de_hash_password(self, password, hashed):
+        if bcrypt.checkpw(password, hashed):
             return True
         else:
             return False
 
+    def authincate_user(self, user, password):
+        response = self.table.scan(FilterExpression=Attr("username").eq(user))
+
+        if (len(response["Items"]) > 0):
+            hased = response['Items'][0]["password"].encode("utf-8")
+            self.de_hash_password(password.encode("utf-8"), hased)
+            verification = self.de_hash_password(password.encode("utf-8"), hased)
+            
+            if (verification):
+                return {
+                    "Result": True,
+                    "Error": None,
+                    "City": response['Items'][0]["currentcity"],
+                    "Country": response['Items'][0]["currentcountry"],
+                    "FirstName": response['Items'][0]["firstname"],
+                    "LastName": response['Items'][0]["lastname"],
+                    "Email": response['Items'][0]["email"],
+                    "Username": response['Items'][0]["username"]
+                }
+            else:
+                return {
+                    "Result": False,
+                    "Error": "Password incorrect",
+                    "City": None,
+                    "Country": None
+                }
+        else:
+            return {
+                "Result": False,
+                "Error": "Username not found",
+                "City": None,
+                "Country": None
+            }
+
+    def delete_account(self, username):
+        response = self.table.scan(FilterExpression = Attr("username").eq(username))
+
+        if len(response["Items"]) > 0:
+            res = self.table.delete_item(Key={self.Primary_Column_Name:username})
+            return {
+                "Result": True,
+                "Error": None,
+                "Description": "Account was deleted"
+                }
+        else:
+            return {
+                    "Result": False,
+                    "Error": "Account does not exist in database"
+                }
+
+    def update_user(self, username, firstname, lastname,  email, city, country, password):
+
+        response = self.table.scan(
+            FilterExpression=Attr("username").eq(username)
+        )
+
+        if password is None:
+            password = response['Items'][0]["password"]
+        else:
+            password = self.hash_password(password)
+
+        if len(response["Items"]) > 0:
+            res = self.table.update_item(
+                Key={
+                    'username': username
+                },
+                UpdateExpression="set city=:d, country=:t, firstname=:l, lastname=:c, email=:s, password=:z",
+                ExpressionAttributeValues={
+                    # ':n': New_BlogName,
+                    ':d': city,
+                    ':t': country,
+                    ':l': firstname,
+                    ':c': lastname,
+                    ':s': email,
+                    ':z': password
+                }
+
+            )
+
+            if res["ResponseMetadata"]["HTTPStatusCode"] == 200:
+                return {
+                    "Result": True,
+                    "Error": None,
+                    "Description": "User was updated successfully",
+                }
+            else:
+                return {
+                    "Result": False,
+                    "Error": "Database error",
+                    "Description": "Database error",
+                }
+
+        else:
+            return {
+                "Result": False,
+                "Error": "DB error",
+                "Description": "User info was not updated"
+            }
     
 
-    def password_verification(self,username,password):
-        response = self.table.scan(FilterExpression=Attr("Username").eq(username))
+    # def password_verification(self,username,password):
+    #     response = self.table.scan(FilterExpression=Attr("Username").eq(username))
         
-        if len(response["Items"]) > 0:
-            original_password = response["Items"][0]["password"]
+    #     if len(response["Items"]) > 0:
+    #         original_password = response["Items"][0]["password"]
 
-            self.de_hash(password.encode("utf-8"), hased)
-            if self.de_hash_password(original_password.encode) == password:
-                return {"Result": True,
-                        "Error": None,
-                        "Description": "Password was verified"}
-            else:
-                return {"Result": False,
-                        "Error": "Password error",
-                        "Description": "Password was not verified"}
-        else:
-            return {"Result": False,
-                        "Error": "Password error",
-                        "Description": "Password was not verified"}
+    #         self.de_hash_password(password.encode("utf-8"), hased)
+    #         if self.de_hash_password(original_password.encode) == password:
+    #             return {"Result": True,
+    #                     "Error": None,
+    #                     "Description": "Password was verified"}
+    #         else:
+    #             return {"Result": False,
+    #                     "Error": "Password error",
+    #                     "Description": "Password was not verified"}
+    #     else:
+    #         return {"Result": False,
+    #                     "Error": "Password error",
+    #                     "Description": "Password was not verified"}
 
     def update_password(self, username, password):
         response = self.table.scan(FilterExpression=Attr("username").eq(username)) #get reponse from DynamoDB
 
         if len(response["Items"]) > 0: # if the response contains a user we bgan to presver dat such as the user city, country, name, etc,
-            
+
             firstname = response["Items"][0]["firstname"]
             lastname = response["Items"][0]["lastname"]
             email = response["Items"][0]['email']
             city = response["Items"][0]["city"]
             country = response["Items"][0]["country"]
-            
           
             response = self.table.put_item(
                 Item = {
@@ -173,89 +247,13 @@ class Users:
                 "Description": "User password was not updated. No such user exists."
             }
 
-    def delete_account(self, username):
-        response = self.table.scan(FilterExpression = Attr("username").eq(username))
-
-        if len(response["Items"]) > 0:
-            res = self.table.delete_item(Key={self.Primary_Column_Name:username})
-            return {
-                "Result": True,
-                "Error": None,
-                "Description": "Account was deleted"
-                }
-        else:
-            return {
-                    "Result": False,
-                    "Error": "Account does not exist in database"
-                }
-
-
-    def hash_pw(self, password):
+    
+    
+    def hash_password(self, password):
         hashed = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
         """
         above hased is a byte stream; below we decode back into a striing and save pw as string
         """
-
         return hashed.decode("utf-8")
 
-    def de_hash(self, password, hashed):
-        if bcrypt.checkpw(password, hashed):
-            return True
-        else:
-            return False
-
-    def hash_password(self, password):
-        hashed = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
-        return hashed.decode("utf-8")
-
-    def de_hash_password(self, password, hashed):
-        if bcrypt.checkpw(password, hashed):
-            return True
-        else:
-            return False
     
-    def authincate_user(self, user, password):
-        response = self.table.scan(
-            FilterExpression=Attr("username").eq(user)
-        )
-
-        ## check if list is emtpy
-        if (len(response["Items"]) > 0):
-            # we have find the user
-            # perform verification
-            hased = response['Items'][0]["password"].encode("utf-8")
-
-            self.de_hash(password.encode("utf-8"), hased)
-
-            verification = self.de_hash(password.encode("utf-8"), hased)
-
-            if (verification):
-                # print("it matches")
-                return {
-                    "Result": True,
-                    "Error": None,
-                    "City": response['Items'][0]["currentcity"],
-                    "Country": response['Items'][0]["currentcountry"],
-                    "FirstName": response['Items'][0]["firstname"],
-                    "LastName": response['Items'][0]["lastname"],
-                    "Email": response['Items'][0]["email"],
-                    "Username": response['Items'][0]["username"]
-                }
-            else:
-                # print("password inncorrect")
-                return {
-                    "Result": False,
-                    "Error": "Password incorrect",
-                    "City": None,
-                    "Country": None
-                }
-
-        else:
-            # that means cant find anythign
-            # print("no such user")
-            return {
-                "Result": False,
-                "Error": "Username not found",
-                "City": None,
-                "Country": None
-            }
