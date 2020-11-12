b168a8631472daba3b217644c1c8478b253d7bd7
anishmoktan <anish.tmngo@gmail.com>
2020-11-12 07:58:13 -0500

    made similar to DevBops

--- EverestCap_Users/users_functions.py
+++ EverestCap_Users/users_functions.py
@@ -13,7 +13,7 @@ class Users:
         self.columns = ["Firstname", "Lastname", "Email", "Password", "City","Country"]   # providing values for the colmuns
         self.table = self.DB.Table(self.__Tablename__)
     
-    def put(self, username, firstname, lastname,  email, city, country, password):
+    def put(self, username, firstname, lastname,  email, city, country, password): #Uploads to DB right away
 
         response = self.table.put_item(
             Item={
@@ -27,9 +27,9 @@ class Users:
             }
         )
 
-    def create_after_verification(self, username, firstname, lastname,  email, city, country, password):
-        if self.username_vacant(username) and self.email_vacant(email):
-            self.put(username, firstname, lastname,  email, city, country, password)
+    def create_after_verification(self, username, firstname, lastname, email, city, country, password):
+        if self.username_vacant(username) and self.email_vacant(email): #Uses True and False from the functions
+            self.put(username, firstname, lastname, email, city, country, password)
             return True
         else:
             return False
@@ -51,31 +51,39 @@ class Users:
         else:
             return False
     
-    def de_hash_password(self, password, hashed):
-        if bcrypt.checkpw(password, hashed):
-            return True
+    def hash_password(self, password):
+        hashed = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()) #Takes in the password and encodes
+        """
+        above hased is a byte stream; below we decode back into a striing and save pw as string
+        """
+        return hashed.decode("utf-8") #Returns the hashed password
+
+    def de_hash_password(self, password, hashed): #Takes in password entered and hashed passowrd from the DB
+        if bcrypt.checkpw(password, hashed): #Compares the two
+            return True #True if they match
         else:
-            return False
+            return False #False if they don't
 
-    def authincate_user(self, user, password):
-        response = self.table.scan(FilterExpression=Attr("username").eq(user))
+    def authincate_user(self, user, password): #Used for logging in the website
+        response = self.table.scan(FilterExpression=Attr("username").eq(user)) #scans DynamoDB for the username and sets it as reponse if found
 
-        if (len(response["Items"]) > 0):
-            hased = response['Items'][0]["password"].encode("utf-8")
-            self.de_hash_password(password.encode("utf-8"), hased)
-            verification = self.de_hash_password(password.encode("utf-8"), hased)
+        if (len(response["Items"]) > 0): #If found
+            hased = response['Items'][0]["password"].encode("utf-8") #Encodes the password in the DB
+            #self.de_hash_password(password.encode("utf-8"), hased)
+            verification = self.de_hash_password(password.encode("utf-8"), hased) #Compares the password entered and password in the DB
             
             if (verification):
                 return {
                     "Result": True,
                     "Error": None,
-                    "City": response['Items'][0]["currentcity"],
-                    "Country": response['Items'][0]["currentcountry"],
-                    "FirstName": response['Items'][0]["firstname"],
+                    "Username": response['Items'][0]["username"], #What is received from DynamoDB
+                    "Firstname": response['Items'][0]["firstname"],
                     "LastName": response['Items'][0]["lastname"],
                     "Email": response['Items'][0]["email"],
-                    "Username": response['Items'][0]["username"]
+                    "City": response['Items'][0]["city"],
+                    "Country": response['Items'][0]["country"]
                 }
+
             else:
                 return {
                     "Result": False,
@@ -83,7 +91,8 @@ class Users:
                     "City": None,
                     "Country": None
                 }
-        else:
+
+        else: #Triggered if username not found in DynamoDB
             return {
                 "Result": False,
                 "Error": "Username not found",
@@ -95,7 +104,7 @@ class Users:
         response = self.table.scan(FilterExpression = Attr("username").eq(username))
 
         if len(response["Items"]) > 0:
-            res = self.table.delete_item(Key={self.Primary_Column_Name:username})
+            res = self.table.delete_item( Key = {self.Primary_Column_Name:username})
             return {
                 "Result": True,
                 "Error": None,
@@ -109,9 +118,7 @@ class Users:
 
     def update_user(self, username, firstname, lastname,  email, city, country, password):
 
-        response = self.table.scan(
-            FilterExpression=Attr("username").eq(username)
-        )
+        response = self.table.scan(FilterExpression=Attr("username").eq(username))
 
         if password is None:
             password = response['Items'][0]["password"]
@@ -120,20 +127,19 @@ class Users:
 
         if len(response["Items"]) > 0:
             res = self.table.update_item(
-                Key={
+                Key = {
                     'username': username
                 },
-                UpdateExpression="set city=:d, country=:t, firstname=:l, lastname=:c, email=:s, password=:z",
-                ExpressionAttributeValues={
+                UpdateExpression = "set firstname=:d, lastname=:t, email=:l, city=:c, country=:s, password=:z",
+                ExpressionAttributeValues = {
                     # ':n': New_BlogName,
-                    ':d': city,
-                    ':t': country,
-                    ':l': firstname,
-                    ':c': lastname,
-                    ':s': email,
+                    ':d': firstname,
+                    ':t': lastname,
+                    ':l': email,
+                    ':c': city,
+                    ':s': country,
                     ':z': password
                 }
-
             )
 
             if res["ResponseMetadata"]["HTTPStatusCode"] == 200:
@@ -177,46 +183,10 @@ class Users:
     #                     "Error": "Password error",
     #                     "Description": "Password was not verified"}
 
-    def update_password(self, username, password):
-        response = self.table.scan(FilterExpression=Attr("username").eq(username)) #get reponse from DynamoDB
+    def update_password(self, username, password): #Updates the DB by first getting back the response
+        response = self.table.scan(FilterExpression=Attr("username").eq(username)) #Sets response at the index for the username
 
-        if len(response["Items"]) > 0: # if the response contains a user we bgan to presver dat such as the user city, country, name, etc,
-
-            firstname = response["Items"][0]["firstname"]
-            lastname = response["Items"][0]["lastname"]
-            email = response["Items"][0]['email']
-            city = response["Items"][0]["city"]
-            country = response["Items"][0]["country"]
-          
-            response = self.table.put_item(
-                Item = {
-                        self.Primary_Column_Name: username,
-                        self.columns[0]: firstname,
-                        self.columns[1]: lastname,
-                        self.columns[2]: email,
-                        self.columns[3]: city,
-                        self.columns[4]: country,
-                        self.columns[5]: self.hash_password(password)
-                    }
-                 )
-            return{
-                "Result": True,
-                "Error": None,
-                "Description": "User password updated"
-            }
-        else:
-            return{
-                "Result": False,
-                "Error": "DB Error",
-                "Description": "User password was not updated. No such user exists."
-            }
-
-    def update_username(self, username, password):
-        response = self.table.scan(
-            FilterExpression=Attr("username").eq(username)
-        )
-
-        if len(response["Items"]) > 0: # if the response contains a user we bgan to presver dat such as the user city, country, name, etc,
+        if len(response["Items"]) > 0: 
 
             firstname = response["Items"][0]["firstname"]
             lastname = response["Items"][0]["lastname"]
@@ -247,13 +217,38 @@ class Users:
                 "Description": "User password was not updated. No such user exists."
             }
 
-    
-    
-    def hash_password(self, password):
-        hashed = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
-        """
-        above hased is a byte stream; below we decode back into a striing and save pw as string
-        """
-        return hashed.decode("utf-8")
+    # def update_username(self, username, password):
+    #     response = self.table.scan(
+    #         FilterExpression=Attr("username").eq(username)
+    #     )
 
-    
+    #     if len(response["Items"]) > 0: # if the response contains a user we bgan to presver dat such as the user city, country, name, etc,
+
+    #         firstname = response["Items"][0]["firstname"]
+    #         lastname = response["Items"][0]["lastname"]
+    #         email = response["Items"][0]['email']
+    #         city = response["Items"][0]["city"]
+    #         country = response["Items"][0]["country"]
+          
+    #         response = self.table.put_item(
+    #             Item = {
+    #                     self.Primary_Column_Name: username,
+    #                     self.columns[0]: firstname,
+    #                     self.columns[1]: lastname,
+    #                     self.columns[2]: email,
+    #                     self.columns[3]: city,
+    #                     self.columns[4]: country,
+    #                     self.columns[5]: self.hash_password(password)
+    #                 }
+    #              )
+    #         return{
+    #             "Result": True,
+    #             "Error": None,
+    #             "Description": "User password updated"
+    #         }
+    #     else:
+    #         return{
+    #             "Result": False,
+    #             "Error": "DB Error",
+    #             "Description": "User password was not updated. No such user exists."
+    #         }
